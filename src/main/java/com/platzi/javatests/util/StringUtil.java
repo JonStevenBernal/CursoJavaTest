4abd9fcebaae6c3387cac5f7b4211966bc1a5716
John Steven Bernal Gonzalez <bernalgonzalezjohn@gmail.com>
2022-03-10 20:19:38 -0500

    Clase Años bisiestos clase 13

--- src/main/java/com/platzi/javatests/util/StringUtil.java
+++ src/main/java/com/platzi/javatests/util/StringUtil.java
@@ -2,7 +2,6 @@ package com.platzi.javatests.util;
 
 public class StringUtil {
     public static String repeat(String str, int times) {
-
         if (times < 0)
             throw new IllegalArgumentException("negative times not allowed");
 
@@ -12,6 +11,10 @@ public class StringUtil {
             result += str;
         }
         return result;
+    }
+
+    public static boolean isEmpty(String str) {
+        return str == null || str.trim().length() <= 0;
     }
 
 }
--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -4,6 +4,9 @@ package com.platzi.javatests.util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class StringUtilTest {
 
     @Test
@@ -24,6 +27,30 @@ public class StringUtilTest {
     @Test(expected = IllegalArgumentException.class)
     public void repeat_string_negative_times() {
         StringUtil.repeat("hola", -1);
+    }
+
+    @Test
+    public void word_is_not_empty(){
+        assertFalse(StringUtil.isEmpty("Hola"));
+    }
+    @Test
+    public void string_not_emphy() {
+        StringUtil.isEmpty(null);
+    }
+    @Test
+    public void quotationMarks_is_Empty(){
+        assertTrue(StringUtil.isEmpty(""));
+    }
+
+    @Test
+    public void space_is_Empty(){
+        String str = new String(" ");
+        assertTrue(StringUtil.isEmpty(str));
+    }
+
+    @Test
+    public void null_is_empty(){
+        assertTrue(StringUtil.isEmpty(null));
     }
 
 }
